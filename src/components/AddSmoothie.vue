a5e067196b464d0a6e7e4dfb2576dddf651b7346
Patrick Peter Zohdy <zohdy@me.com>
2018-05-04 17:35:07 +0200

    functionality to store a new smoothie locally

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form @submit.prevent="addSmoothie">
+      <div class="field title">
+        <label for="title">Smoothie Title</label>
+        <input type="text" name="title" v-model="title">
+      </div>
+      <div v-for="(ingredient, index) in ingredients" :key="index">
+        <label for="ingredient">Ingredients:</label>
+        <input type="text" name="ingredient" v-model="ingredients[index]">
+      </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient">Add an ingredient</label>
+        <input type="text" name="ingredient" @keydown.tab.prevent="addIngredient" v-model="newIngredient">
+      </div>
+      <div class="field center-align">
+        <p class="red-text" v-if="feedback">{{ feedback }}</p>
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AddSmoothie',
+    data(){
+      return {
+        title: null,
+        newIngredient: null,
+        ingredients: [],
+        feedback: null
+      }
+    },
+    methods: {
+      addSmoothie(){
+        console.log(this.title, this.ingredients);
+      },
+      addIngredient(){
+        if(this.newIngredient){ // If not null
+          this.ingredients.push(this.newIngredient);
+          this.newIngredient = null; // reset textfield
+          this.feedback = null;
+        } else {
+          this.feedback = 'You must enter a value to add an ingredient'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .add-smoothie {
+    margin-top: 60px;
+    padding: 20px;
+    max-width: 500px;
+  }
+
+  .add-smoothie h2 {
+    font-size: 2em;
+    margin: 20px auto;
+  }
+
+  .add-smoothie .field {
+    margin: 20px auto;
+  }
+
+</style>
